870c6154eea19dac12717fd6b7f04b7c6e57445c
Neelesh Goyal <89386582+neelesh98965@users.noreply.github.com>
2025-05-04 11:18:49 +0530

    api changes

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -18,7 +18,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import axios from 'axios';
 
 // Configure axios defaults
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://sbvp-backend-python-with-sqllite-a2rq.onrender.com';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 const AdminPage = () => {
